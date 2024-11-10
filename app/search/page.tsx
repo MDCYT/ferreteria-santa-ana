d19377aca3199c3852d5c5d89e2a99065a080635
Jose Ortiz <me@mdcdev.me>
2024-11-09 19:59:35 -0500

    Update page.tsx

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,33 +1,35 @@
 "use client";
 
-import { ThemeProvider } from "next-themes";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-import { useSearchParams } from 'next/navigation'
-import { useRouter } from 'next/navigation'
-import SearchPage from "@/components/SearchPage";
+import { Suspense } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { ThemeProvider } from 'next-themes';
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+import SearchPage from '@/components/SearchPage';
 
-export default function Home() {
-
+const Page = () => {
+  const router = useRouter();
   const searchParams = useSearchParams()
-  const router = useRouter()
-
-  const search = searchParams.get('q')
+  const search = searchParams.get('q');
 
   if (search) {
     return (
       <ThemeProvider attribute="class">
         <div className="min-h-screen flex flex-col">
-          <Header/>
+          <Header />
           <main className="flex-grow">
-            <SearchPage searchQuery={search} />
+            <Suspense fallback={<div>Loading...</div>}>
+              <SearchPage searchQuery={Array.isArray(search) ? search[0] : search} />
+            </Suspense>
           </main>
           <Footer />
         </div>
       </ThemeProvider>
     );
   } else {
-    router.push('/products')
-    return null
+    router.push('/products');
+    return null;
   }
-}
+};
+
+export default Page;
