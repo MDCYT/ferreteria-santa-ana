dc9cd75c4d8e8c2a99939806684ddf75fc18bd3e
Jose Ortiz <me@mdcdev.me>
2024-11-09 20:03:04 -0500

    Update page.tsx

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -8,27 +8,31 @@ import Footer from '@/components/Footer';
 import SearchPage from '@/components/SearchPage';
 
 const Page = () => {
-  const router = useRouter();
-  const searchParams = useSearchParams()
+
+  return (
+    <ThemeProvider attribute="class">
+      <div className="min-h-screen flex flex-col">
+        <Header />
+        <main className="flex-grow">
+          <Suspense fallback={<div>Loading...</div>}>
+            <SearchComponent />
+          </Suspense>
+        </main>
+        <Footer />
+      </div>
+    </ThemeProvider>
+  );
+};
+
+const SearchComponent = () => {
+  const searchParams = useSearchParams();
   const search = searchParams.get('q');
+  const router = useRouter();
 
   if (search) {
-    return (
-      <ThemeProvider attribute="class">
-        <div className="min-h-screen flex flex-col">
-          <Header />
-          <main className="flex-grow">
-            <Suspense fallback={<div>Loading...</div>}>
-              <SearchPage searchQuery={Array.isArray(search) ? search[0] : search} />
-            </Suspense>
-          </main>
-          <Footer />
-        </div>
-      </ThemeProvider>
-    );
+    return <SearchPage searchQuery={search} />;
   } else {
     router.push('/products');
-    return null;
   }
 };
 
